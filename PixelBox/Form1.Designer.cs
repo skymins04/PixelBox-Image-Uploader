5133aa9409c811b7cb59f7f7d0f5f29e48d6d52c
skymins04 <skymin0417@gmail.com>
2019-09-07 01:13:26 +0900

    기본적인 틀 제작. 이미지 리스트 관리, 아두이노 없로드 기능 구현 완료

--- PixelBox/Form1.Designer.cs
+++ PixelBox/Form1.Designer.cs
@@ -28,13 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.AddImgBtn = new System.Windows.Forms.Button();
+            this.DelImgBtn = new System.Windows.Forms.Button();
+            this.UpBtn = new System.Windows.Forms.Button();
+            this.DownBtn = new System.Windows.Forms.Button();
+            this.UploadBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AddImgBtn
+            // 
+            this.AddImgBtn.Location = new System.Drawing.Point(289, 27);
+            this.AddImgBtn.Name = "AddImgBtn";
+            this.AddImgBtn.Size = new System.Drawing.Size(85, 23);
+            this.AddImgBtn.TabIndex = 1;
+            this.AddImgBtn.Text = "이미지 추가";
+            this.AddImgBtn.UseVisualStyleBackColor = true;
+            this.AddImgBtn.Click += new System.EventHandler(this.AddImgBtn_Click);
+            // 
+            // DelImgBtn
+            // 
+            this.DelImgBtn.Location = new System.Drawing.Point(289, 56);
+            this.DelImgBtn.Name = "DelImgBtn";
+            this.DelImgBtn.Size = new System.Drawing.Size(85, 23);
+            this.DelImgBtn.TabIndex = 2;
+            this.DelImgBtn.Text = "이미지 삭제";
+            this.DelImgBtn.UseVisualStyleBackColor = true;
+            this.DelImgBtn.Click += new System.EventHandler(this.DelImgBtn_Click);
+            // 
+            // UpBtn
+            // 
+            this.UpBtn.Location = new System.Drawing.Point(288, 85);
+            this.UpBtn.Name = "UpBtn";
+            this.UpBtn.Size = new System.Drawing.Size(85, 23);
+            this.UpBtn.TabIndex = 3;
+            this.UpBtn.Text = "순서 올리기";
+            this.UpBtn.UseVisualStyleBackColor = true;
+            this.UpBtn.Click += new System.EventHandler(this.UpBtn_Click);
+            // 
+            // DownBtn
+            // 
+            this.DownBtn.Location = new System.Drawing.Point(288, 114);
+            this.DownBtn.Name = "DownBtn";
+            this.DownBtn.Size = new System.Drawing.Size(85, 23);
+            this.DownBtn.TabIndex = 4;
+            this.DownBtn.Text = "순서 내리기";
+            this.DownBtn.UseVisualStyleBackColor = true;
+            this.DownBtn.Click += new System.EventHandler(this.DownBtn_Click);
+            // 
+            // UploadBtn
+            // 
+            this.UploadBtn.Location = new System.Drawing.Point(289, 176);
+            this.UploadBtn.Name = "UploadBtn";
+            this.UploadBtn.Size = new System.Drawing.Size(84, 60);
+            this.UploadBtn.TabIndex = 7;
+            this.UploadBtn.Text = "업로드";
+            this.UploadBtn.UseVisualStyleBackColor = true;
+            this.UploadBtn.Click += new System.EventHandler(this.UploadBtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 158);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(271, 78);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "옵션";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "포트";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(107, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(113, 20);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.Text = "===포트 선택===";
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(270, 140);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "이미지 리스트";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(6, 20);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(258, 112);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(386, 249);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.UploadBtn);
+            this.Controls.Add(this.DownBtn);
+            this.Controls.Add(this.UpBtn);
+            this.Controls.Add(this.DelImgBtn);
+            this.Controls.Add(this.AddImgBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PixelBox 업로더";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button AddImgBtn;
+        private System.Windows.Forms.Button DelImgBtn;
+        private System.Windows.Forms.Button UpBtn;
+        private System.Windows.Forms.Button DownBtn;
+        private System.Windows.Forms.Button UploadBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- PixelBox/Form1.cs
+++ PixelBox/Form1.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,125 @@ namespace PixelBox
 {
     public partial class Form1 : Form
     {
+        List<string> imgList = new List<string>();
+        const int MAX_IMG = 6;
+        int imgCount = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void DelImgBtn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+            imgList.Remove(imgList[listBox1.SelectedIndex]);
+            imgCount -= 1;
+        }
+
+        private void UpBtn_Click(object sender, EventArgs e)
+        {
+            int idx = listBox1.SelectedIndex;
+            if(idx >= 1)
+            {
+                object obj = listBox1.SelectedItem;
+                string imgPath = imgList[idx];
+
+                imgList.Remove(imgList[idx]);
+                imgList.Insert(idx - 1, imgPath);
+
+                listBox1.Items.Remove(listBox1.SelectedItem);
+                listBox1.Items.Insert(idx - 1, obj);
+                listBox1.SetSelected(idx - 1, true);
+            }
+        }
+
+        private void DownBtn_Click(object sender, EventArgs e)
+        {
+            int idx = listBox1.SelectedIndex;
+            if (!(idx == listBox1.Items.Count-1 || listBox1.SelectedItem == null))
+            {
+                object obj = listBox1.SelectedItem;
+                string imgPath = imgList[idx];
+
+                imgList.Remove(imgList[idx]);
+                imgList.Insert(idx + 1, imgPath);
+
+                listBox1.Items.Remove(listBox1.SelectedItem);
+                listBox1.Items.Insert(idx + 1, obj);
+                listBox1.SetSelected(idx + 1, true);
+            }
+        }
+
+        private void AddImgBtn_Click(object sender, EventArgs e)
+        {
+            if(imgCount < 6)
+            {
+                OpenFileDialog openFile = new OpenFileDialog();
+                openFile.DefaultExt = "png";
+                openFile.Filter = "PNG 이미지(*.png; *.PNG)|*.png;*.PNG";
+                openFile.ShowDialog();
+                if (openFile.FileName.Length > 0)
+                {
+                    listBox1.Items.Add(Path.GetFileName(openFile.FileName));
+                    imgList.Add(openFile.FileName);
+
+                    imgCount += 1;
+                }
+            }
+            else
+            {
+                SystemSounds.Beep.Play();
+                MessageBox.Show("이미지는 최대 6장까지만\n업로드 할 수 있습니다.", "이미지 개수 초과");
+            }
+        }
+
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+
+            string[] comlist = System.IO.Ports.SerialPort.GetPortNames();
+            if (comlist.Length > 0) comboBox1.Items.AddRange(comlist);
+        }
+
+        private void UploadBtn_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo png2ino = new ProcessStartInfo();
+            ProcessStartInfo arduinoUpload = new ProcessStartInfo();
+            Process p = new Process();
+
+            string png2inoArg = null;
+            foreach (string path in imgList){
+                png2inoArg += " " + path;
+            }
+
+            png2ino.CreateNoWindow = true;
+            png2ino.UseShellExecute = false;
+            png2ino.FileName = ".\\rgbmatrix\\png2ino.exe";
+            png2ino.WindowStyle = ProcessWindowStyle.Hidden;
+            png2ino.Arguments = png2inoArg;
+
+            p.StartInfo = png2ino;
+            p.Start();
+            p.WaitForExit();
+
+            File.Delete(".\\rgbmatrix\\data.h");
+            File.Move(".\\data.h", ".\\rgbmatrix\\data.h");
+
+            arduinoUpload.CreateNoWindow = false;
+            arduinoUpload.UseShellExecute = false;
+            arduinoUpload.FileName = ".\\arduino-1.8.9\\arduino_debug.exe";
+            arduinoUpload.WindowStyle = ProcessWindowStyle.Hidden;
+            arduinoUpload.Arguments = "--board arduino:avr:nano --port " + comboBox1.Text + " -v --upload .\\rgbmatrix\\rgbmatrix.ino";
+
+            p.StartInfo = arduinoUpload;
+            p.Start();
+            p.WaitForExit();
+        }
+
+        private void DelayTbox_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
